e18ea4ef68b8855f6d23ee85e37de625c2104f4b
ziad-ahmed-56 <ziadelrehany2002@hotmail.com>
2022-09-14 22:26:14 +0200

    push

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'f1cce2de-3b4b-4663-acd5-86850e08541e';
+export const MODAL_CLOSE_SEC = 2.5;
